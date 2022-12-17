952974bf740d0c442a8a0c8b2502ceba925c3601
Gabriel Matei <gabrielvmatei@gmail.com>
2022-12-17 22:14:10 +0200

    footer css

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -3,7 +3,7 @@ import { Footer } from './Footer';
 
 export const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='bg-light d-flex flex-column flex-fill wrapper'>
+    <div className='d-flex flex-column flex-fill wrapper'>
       <main className='d-flex flex-column flex-grow-1'>{children}</main>
       <Footer />
     </div>
