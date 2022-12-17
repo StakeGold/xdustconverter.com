f8e2a6baee9532143c8c19dc8f275ba871ee6ec5
Gabriel Matei <gabrielvmatei@gmail.com>
2022-12-17 22:36:40 +0200

    card layout

--- src/pages/Convert/components/ConvertLayout.tsx
+++ src/pages/Convert/components/ConvertLayout.tsx
@@ -2,10 +2,18 @@ import React from 'react';
 
 export const ConvertLayout = ({ children }: React.PropsWithChildren) => {
   return (
-    <div className='container py-4'>
+    <div className='container'>
       <div className='row'>
-        <div className='col-12 col-md-10 mx-auto'>
-          <div>{children}</div>
+        <div className='col-12 col-md-9 col-lg-7 col-xl-6 mx-auto text-center my-spacer'>
+          <h1 className='page-title'>xDustConverter</h1>
+          <h3 className='page-description font-weight-light text-secondary'>
+            Convert small ESDT amounts to WEGLD
+          </h3>
+        </div>
+      </div>
+      <div className='row'>
+        <div className='col-12 col-md-9 col-lg-7 col-xl-6 mx-auto'>
+          <div className='card'>{children}</div>
         </div>
       </div>
     </div>
