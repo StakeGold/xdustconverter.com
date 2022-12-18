cec53e6544fa40cce676df7433075c1ca6fe6ed7
Gabriel Matei <gabrielvmatei@gmail.com>
2022-12-18 20:35:45 +0200

    update configs

--- src/config.devnet.tsx
+++ src/config.devnet.tsx
@@ -1,13 +1,8 @@
 import { EnvironmentsEnum } from '@elrondnetwork/dapp-core/types';
 
-export const contractAddress =
-  'erd1qqqqqqqqqqqqqpgq72l6vl07fkn3alyfq753mcy4nakm0l72396qkcud5x';
-
+export const ENVIRONMENT = EnvironmentsEnum.devnet;
 export const UPDATE_REFRESH_RATE = 60000;
-
-// Generate your own WalletConnect 2 ProjectId here: https://cloud.walletconnect.com/app
-export const walletConnectV2ProjectId = '9b1a9564f91cb659ffe21b73d5c4e2d8';
-
-export const apiTimeout = 6000;
-
-export const environment = EnvironmentsEnum.mainnet;
+export const API_TIMEOUT = 6000;
+export const CONTRACT_ADDRESS =
+  'erd1qqqqqqqqqqqqqpgq72l6vl07fkn3alyfq753mcy4nakm0l72396qkcud5x';
+export const WALLET_CONNECT_V2_PROJECT_ID = '9c1851da02319e1280cbbf2de5d8ee46';
