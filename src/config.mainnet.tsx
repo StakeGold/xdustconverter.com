8b47fb72eb2d9906203080bbf0c542056bc4217f
Gabriel Matei <gabrielvmatei@gmail.com>
2022-12-20 14:18:50 +0200

    update token

--- src/config.mainnet.tsx
+++ src/config.mainnet.tsx
@@ -7,4 +7,4 @@ export const CONTRACT_ADDRESS =
   'erd1qqqqqqqqqqqqqpgq72l6vl07fkn3alyfq753mcy4nakm0l72396qkcud5x';
 export const WALLET_CONNECT_V2_PROJECT_ID = '9c1851da02319e1280cbbf2de5d8ee46';
 export const WEGLD_ID = 'WEGLD-bd4d79';
-export const USDC_ID = 'USDC-8d4068';
+export const USDC_ID = 'USDC-c76f1f';
