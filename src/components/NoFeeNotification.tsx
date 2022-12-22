d4016076720982f47f253de229da4c0f468dcc3f
Gabriel Matei <gabrielvmatei@gmail.com>
2022-12-22 11:18:02 +0200

    update promotion

--- src/components/NoFeeNotification.tsx
+++ src/components/NoFeeNotification.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 const NoFeeNotification = () => {
   return (
-    <div className='d-flex justify-content-center align-items-center px-3 py-3 px-md-4 bg-primary text-dark font-weight-bold text-center'>
-      ☃️ CHRISTMAS EDITION ☃️ <br />
-      0% fees until 26.12.2022
+    <div className='d-flex flex-column justify-content-center align-items-center px-3 py-3 px-md-4 bg-primary text-dark font-weight-bold text-center'>
+      <div>☃️ CHRISTMAS EDITION ☃️</div>
+      <div>
+        <del className='text-secondary'>5%</del> 0% fees until 26.12.2022
+      </div>
     </div>
   );
 };
