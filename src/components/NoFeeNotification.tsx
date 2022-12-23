ffcd45b938d49bbcfabc333230d932d0d4b6c783
Marius Stoica <stoica.nmarius@yahoo.ro>
2022-12-23 09:42:52 +0200

    Improved NoFeeNotification text

--- src/components/NoFeeNotification.tsx
+++ src/components/NoFeeNotification.tsx
@@ -4,7 +4,7 @@ const NoFeeNotification = () => {
   return (
     <div className='d-flex justify-content-center align-items-center px-3 py-3 px-md-4 bg-primary text-dark font-weight-bold text-center'>
       ☃️ CHRISTMAS EDITION ☃️ <br />
-      0% fees until 26.12.2022
+      0% protocol fees until 26.12.2022
     </div>
   );
 };
