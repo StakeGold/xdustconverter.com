eb1e4e105aa263a28064d9c3b6e90f6997e90ec2
Marius Stoica <stoica.nmarius@yahoo.ro>
2022-12-23 09:55:15 +0200

    Improved NoFeeNotification text (#37)

--- src/components/NoFeeNotification.tsx
+++ src/components/NoFeeNotification.tsx
@@ -5,7 +5,8 @@ const NoFeeNotification = () => {
     <div className='d-flex flex-column justify-content-center align-items-center px-3 py-3 px-md-4 bg-primary text-dark font-weight-bold text-center'>
       <div>☃️ CHRISTMAS EDITION ☃️</div>
       <div>
-        <del className='text-secondary'>5%</del> 0% fees until 26.12.2022
+        <del className='text-secondary'>5%</del> 0% protocol fees until
+        26.12.2022
       </div>
     </div>
   );
