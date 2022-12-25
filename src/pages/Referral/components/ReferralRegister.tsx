cd6481e3cc9103f00054d50f8712a7bb20819a7d
Gabriel Matei <gabrielvmatei@gmail.com>
2022-12-25 18:49:41 +0200

    register ui

--- src/pages/Referral/components/ReferralRegister.tsx
+++ src/pages/Referral/components/ReferralRegister.tsx
@@ -1,8 +1,19 @@
-import React from 'react';
-import { OverlayTrigger, Tooltip } from 'react-bootstrap';
+import React, { useMemo, useState } from 'react';
+import { OverlayTrigger, Spinner, Tooltip } from 'react-bootstrap';
 import { ReactComponent as InfoIcon } from 'assets/img/info.svg';
 
 export const ReferralRegister = () => {
+  const [tag, setTag] = useState('');
+
+  const isLoading = false;
+  const isDisabled = useMemo(() => {
+    return tag.length === 0;
+  }, [tag]);
+
+  const handleSubmit = () => {
+    // TODO
+  };
+
   const InfoTooltip = () => {
     return (
       <OverlayTrigger
@@ -21,15 +32,51 @@ export const ReferralRegister = () => {
     );
   };
 
+  const FeeTooltip = () => {
+    return (
+      <OverlayTrigger
+        placement='top'
+        overlay={(props) => (
+          <Tooltip {...props}>
+            {/* TODO */}
+            <p> </p>
+          </Tooltip>
+        )}
+      >
+        <InfoIcon style={{ width: '1.25rem', height: '1.25rem' }} />
+      </OverlayTrigger>
+    );
+  };
+
   return (
     <div className='card mb-4'>
       <h4 className='mb-4'>
         Create referral tag <InfoTooltip />
       </h4>
-      <p className='font-italic'></p>
-      <p>Tag: ....</p>
-      <p>Fee percent: 5%</p>
-      <button>Register</button>
+      <div className='mb-4'>
+        <label>Referral tag</label>
+        <input
+          className='referral-tag-input'
+          type='text'
+          placeholder='Referral tag..'
+          value={tag}
+          onChange={(e) => setTag(e.target.value)}
+        />
+      </div>
+      <div className='mb-4'>
+        Referral rewards percent: 5% <FeeTooltip />
+      </div>
+      <button
+        className='btn btn-primary btn-connect'
+        onClick={handleSubmit}
+        disabled={isLoading || isDisabled}
+      >
+        {isLoading ? (
+          <Spinner as='span' animation='border' size='sm' />
+        ) : (
+          'Create referral tag'
+        )}
+      </button>
     </div>
   );
 };
