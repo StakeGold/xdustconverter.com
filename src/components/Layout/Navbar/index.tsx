bbf80e9a09ffb15f48f9f4d095757bc99302766a
Gabriel Matei <gabrielvmatei@gmail.com>
2022-12-25 17:10:48 +0200

    navbar css

--- src/components/Layout/Navbar/index.tsx
+++ src/components/Layout/Navbar/index.tsx
@@ -1,22 +1,12 @@
 import React from 'react';
-import { useGetIsLoggedIn } from '@elrondnetwork/dapp-core/hooks';
-import { logout } from '@elrondnetwork/dapp-core/utils';
-import { Navbar as BsNavbar, NavItem, Nav } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import { routeNames } from 'routes';
 
 export const Navbar = () => {
-  const isLoggedIn = useGetIsLoggedIn();
-
-  const handleLogout = () => {
-    logout(window.location.origin);
-  };
-
   return (
     <div className='mx-auto my-4'>
-      <NavLink to={routeNames.home} className='mr-4'>
-        Home
-      </NavLink>
+      <NavLink to={routeNames.home}>Home</NavLink>
+      <span className='text-primary mx-3'>•</span>
       <NavLink to={routeNames.referral}>Referral</NavLink>
     </div>
   );
