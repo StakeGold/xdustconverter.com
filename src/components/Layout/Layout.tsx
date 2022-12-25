c946612cfcbbbdda95c16fa284e7488659bfc6b8
Gabriel Matei <gabrielvmatei@gmail.com>
2022-12-25 14:56:31 +0200

    refactor app layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import logo from 'assets/img/xdustconverter_mas.png';
 import { ReferralNotification } from 'components/Notifications';
+import { Welcome } from 'components/Welcome';
+import { routeNames } from 'routes';
 import { Footer } from './Footer';
 
 export const Layout = ({ children }: { children: React.ReactNode }) => {
@@ -12,7 +16,29 @@ export const Layout = ({ children }: { children: React.ReactNode }) => {
     <>
       {notifications}
       <div className='d-flex flex-column flex-fill wrapper'>
-        <main className='d-flex flex-column flex-grow-1'>{children}</main>
+        <main className='d-flex flex-column flex-grow-1'>
+          <div className='container'>
+            <div className='row'>
+              <div className='col-12 col-md-12 col-lg-9 col-xl-8 mx-auto text-center my-spacer'>
+                <NavLink to={routeNames.home}>
+                  <h1 className='page-title'>
+                    <span>xDustConverter</span>
+                    <img src={logo} />
+                  </h1>
+                </NavLink>
+                <h3 className='page-description font-weight-light text-secondary'>
+                  Convert small token amounts to WEGLD
+                </h3>
+              </div>
+            </div>
+            <div className='row'>
+              <div className='col-12 col-md-12 col-lg-9 col-xl-8 mx-auto'>
+                <Welcome />
+                {children}
+              </div>
+            </div>
+          </div>
+        </main>
         <Footer />
       </div>
     </>
--- src/components/Notifications/ReferralNotification.tsx
+++ src/components/Notifications/ReferralNotification.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { useGetAccount } from '@elrondnetwork/dapp-core/hooks';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import { routeNames } from 'routes';
 
 const ReferralNotification = () => {
   const { address } = useGetAccount();
   const isLoggedIn = Boolean(address);
 
+  const location = useLocation();
+  const isReferralPage = location.pathname === routeNames.referral;
+
   return (
     <>
-      {isLoggedIn && (
+      {isLoggedIn && !isReferralPage && (
         <div className='d-flex flex-column justify-content-center align-items-center px-3 py-3 px-md-4 bg-primary text-dark font-weight-bold text-center'>
           <NavLink
             to={routeNames.referral}
