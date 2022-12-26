b3f49f1818e65bbdf5c8c18904eacc3fbb274bfe
Marius Stoica <stoica.nmarius@yahoo.ro>
2022-12-26 10:49:28 +0200

    Show hero tag if available (#38)

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -6,7 +6,7 @@ import { faPowerOff } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const Welcome = () => {
-  const { address } = useGetAccount();
+  const { address, username } = useGetAccount();
   const isLoggedIn = Boolean(address);
 
   const handleLogout = () => {
@@ -21,7 +21,11 @@ export const Welcome = () => {
     <div className='card welcome-card mb-4'>
       <h3>
         Welcome, <br />
-        <ExplorerLink page={`/accounts/${address}`} text={address} />!
+        <ExplorerLink
+          page={`/accounts/${address}`}
+          text={username ?? address}
+        />
+        !
       </h3>
       <button className='btn btn-secondary btn-logout' onClick={handleLogout}>
         <FontAwesomeIcon icon={faPowerOff} />
