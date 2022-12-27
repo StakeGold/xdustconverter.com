ce4c976800f75e5ba6e1bd7a88960377689d15ea
Gabriel Matei <gabrielvmatei@gmail.com>
2022-12-27 12:14:40 +0200

    extend no fee period

--- src/components/NoFeeNotification.tsx
+++ src/components/NoFeeNotification.tsx
@@ -3,10 +3,10 @@ import React from 'react';
 const NoFeeNotification = () => {
   return (
     <div className='d-flex flex-column justify-content-center align-items-center px-3 py-3 px-md-4 bg-primary text-dark font-weight-bold text-center'>
-      <div>☃️ CHRISTMAS EDITION ☃️</div>
+      <div>🎉 Happy New Year! 🎉</div>
       <div>
-        <del className='text-secondary'>5%</del> 0% protocol fees until
-        26.12.2022
+        <del className='text-secondary'>5%</del> 0% protocol fees until end of
+        year
       </div>
     </div>
   );
