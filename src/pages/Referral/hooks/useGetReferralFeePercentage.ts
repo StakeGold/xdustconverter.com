e1f632f477312f5575d891c34f292a4243cf5cd0
StefanM55 <stefanmiloiu@live.com>
2023-01-27 19:12:40 +0200

    backward compatibility assured

--- src/pages/Referral/hooks/useGetReferralFeePercentage.ts
+++ src/pages/Referral/hooks/useGetReferralFeePercentage.ts
@@ -2,13 +2,11 @@ import React from 'react';
 import { useQuery } from '@apollo/client';
 import { REFERRAL_INFO } from 'api/queries';
 
-export const useGetReferralFeePercentage = () => {
+export const useGetReferralFeePercentage = (): { feePercent: number } => {
   const { data } = useQuery(REFERRAL_INFO);
 
   return React.useMemo(() => {
     const feePercent = data.accountReferralInfo.feePercentage;
-    return {
-      feePercent
-    };
+    return feePercent;
   }, [data]);
 };
