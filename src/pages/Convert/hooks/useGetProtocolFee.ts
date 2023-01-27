2060d9a184f8939a69e7a1411d736e2604104126
StefanM55 <stefanmiloiu@live.com>
2023-01-27 22:07:25 +0200

    added return type in useGetProtocolFee

--- src/pages/Convert/hooks/useGetProtocolFee.ts
+++ src/pages/Convert/hooks/useGetProtocolFee.ts
@@ -2,7 +2,7 @@ import React from 'react';
 import { useQuery } from '@apollo/client';
 import { PROTOCOL_FEE } from '../../../api/queries';
 
-export const useGetProtocolFee = () => {
+export const useGetProtocolFee = (): { feePercent: number } => {
   const { data, loading, error } = useQuery(PROTOCOL_FEE);
 
   const feePercent = data?.protocolFeePercent;
