a444bf564bc18ac460de88df1a4a54514e605382
Gabriel Matei <gabrielvmatei@gmail.com>
2023-03-02 20:51:30 +0200

    fix transactions

--- src/apiCalls/transactions.tsx
+++ src/apiCalls/transactions.tsx
@@ -2,6 +2,7 @@ import { Transaction } from '@multiversx/sdk-core/out';
 import { sendTransactions } from '@multiversx/sdk-dapp/services';
 import { TransactionsDisplayInfoType } from '@multiversx/sdk-dapp/types';
 import { refreshAccount } from '@multiversx/sdk-dapp/utils';
+
 export const sendAndSignTransactions = async (
   transactions: Transaction[],
   transactionsDisplayInfo: TransactionsDisplayInfoType,
@@ -13,11 +14,15 @@ export const sendAndSignTransactions = async (
 }> => {
   try {
     await refreshAccount();
+
+    const parsedTransactions = transactions.map((t) => t.toPlainObject());
+
     const { sessionId, error } = await sendTransactions({
-      transactions: transactions,
+      transactions: parsedTransactions,
       transactionsDisplayInfo,
       minGasLimit
     });
+
     await refreshAccount();
     return { success: error !== undefined, error: error ?? '', sessionId };
   } catch (error: any) {
