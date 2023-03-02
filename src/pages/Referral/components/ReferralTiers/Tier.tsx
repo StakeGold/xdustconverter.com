6643f9a38bf0b7b592a194f531634d2c0a860eec
Gabriel Matei <gabrielvmatei@gmail.com>
2023-03-02 17:24:38 +0200

    fix imports

--- src/pages/Referral/components/ReferralTiers/Tier.tsx
+++ src/pages/Referral/components/ReferralTiers/Tier.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { FormatAmount } from '@multiversx/sdk-dapp/UI';
 import { faLock } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FormatAmount } from '@multiversx/sdk-dapp/UI';
 import { ReferralDetails, TierDetails } from 'types';
 
 interface TierProps {
