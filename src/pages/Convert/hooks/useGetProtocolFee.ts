f2b9cd6fb7b45ab90c06abb04ad8eccc14e34f39
Marius Stoica <stoica.nmarius@yahoo.ro>
2023-05-03 11:41:13 +0300

    Revert slippage is amount is greater than min

--- src/pages/Convert/hooks/useGetProtocolFee.ts
+++ src/pages/Convert/hooks/useGetProtocolFee.ts
@@ -5,7 +5,7 @@ import { PROTOCOL_FEE } from '../../../api/queries';
 export const useGetProtocolFee = (): number => {
   const { data, loading, error } = useQuery(PROTOCOL_FEE);
 
-  const feePercent = data?.protocolFeePercent;
+  const feePercent = data?.protocolFeePercent ?? 0;
 
   return React.useMemo(() => {
     return feePercent;
